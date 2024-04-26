96b6efb9c6bea99d4b91569d55c72136b0beb683
Nikita Diser <nikita.diser@googlemail.com>
2024-04-26 16:56:58 +0200

    TreeNode.h class definition

--- TreeNode.h
+++ TreeNode.h
@@ -12,17 +12,38 @@ class TreeNode
 {
 
 private:
-    ///////////////////////////////////////
-    // Ihr Code hier:
+    int m_NodeOrderID;
+    int m_NodeChronologicalID;
+    string m_Name;
+    int m_Age;
+    double m_Income;
+    int m_Postcode;
 
-    //
-    ////////////////////////////////////
+    TreeNode* m_Left;
+    TreeNode* m_Right;
 
 public:
-    ///////////////////////////////////////
-    // Ihr Code hier:
-    TreeNode(int, int, string, int, double, int);
+    TreeNode(int nodeOrderID, int nodeChronologicalID, string name, int age, double income, int postcode);
 
-    //
-    ////////////////////////////////////
+    int getNodeOrderID();
+    int getNodeChronologicalID();
+    string getName();
+    int getAge();
+    double getIncome();
+    int getPostcode();
+
+    TreeNode* getLeft();
+    TreeNode* getRight();
+
+    void setNodeOrderID(int nodeOrderID);
+    void setNodeChronologicalID(int nodeChronologicalID);
+    void setName(string name);
+    void setAge(int age);
+    void setIncome(double income);
+    void setPostcode(int postcode);
+
+    void setLeft(TreeNode* left);
+    void setRight(TreeNode* right);
+
+    void print();
 };
