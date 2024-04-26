53540e38794db1018f0958dec8645c267c955317
Nikita Diser <nikita.diser@googlemail.com>
2024-04-26 17:24:57 +0200

    Tree.cpp addNodes hinzugefügt

--- Tree.cpp
+++ Tree.cpp
@@ -10,8 +10,53 @@
 
 using namespace std;
 
-////////////////////////////////////
-// Ihr Code hier:
+Tree::Tree()
+{
+    m_anker = nullptr;
+    m_CurrentNodeChronologicalID = 0;
+}
 
-//
-////////////////////////////////////
+Tree::~Tree()
+{
+    //TODO: Delete des ganzes Baumes
+}
+
+void Tree::addNode(string name, int age, double income, int postcode)
+{
+    int nodeOrderID = age + income + postcode;
+
+    TreeNode* newNode = new TreeNode(nodeOrderID, m_CurrentNodeChronologicalID, name, age, income, postcode);
+    m_CurrentNodeChronologicalID++;
+    //Tree ist leer
+    if (m_anker == nullptr) m_anker = newNode;
+    //Tree ist nicht leer
+    else
+    {
+        TreeNode* current = m_anker;
+        
+        while (true)
+        {
+            if (newNode->getNodeOrderID() < current->getNodeOrderID())
+            {
+                if (current->getLeft() == nullptr)
+                {
+                    current->setLeft(newNode);
+                    break;
+                }
+                else current = current->getLeft();
+            }
+            else
+            {
+                if (current->getRight() == nullptr)
+                {
+                    current->setRight(newNode);
+                    break;
+                }
+                else current = current->getRight();
+            }
+        }
+        
+    
+        
+    }
+}
